f3b6843e276f4b5b49028808bcc0d6da5e2432d0
Ankush <ankushagrawal4321@gmail.com>
2022-07-23 14:46:34 +0530

    our contact list is ready to use

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const port = 8000;
 const path = require('path');
 
+const db = require('./config/mongoose');
+const Contact = require('./modals/contact');
+
 const app = express();
 
 app.set('view engine','ejs');
@@ -22,10 +25,19 @@ var contactList = [
 
 app.get('/',function(req,res){
 
-  return res.render('home',{
-    title:"My Contact List",
-    contact_list: contactList
-  });
+    Contact.find({},function(err,contacts){
+        if(err){
+            console.log('Error in fetching contacts');
+            return;
+        }
+        return res.render('home',{
+            title:"My Contact List",
+            contact_list: contacts
+          });
+    })
+
+
+  
 }); 
 
 app.get('/practice',function(req,res){
@@ -42,20 +54,40 @@ app.post('/contact-list',function(req,res){
     //     name: req.body.name,
     //     phone: req.body.phone
     // });
-    contactList.push(req.body);
+    // contactList.push(req.body);
+    Contact.create({
+        name:req.body.name,
+        phone:req.body.phone
+    },function(err,newContact){
+        if(err){console.log('error in creating a contact');
+        return;}
+
+        console.log('*********',newContact);
+        return res.redirect('back');
+    });
 
     // return res.redirect('/');
-    return res.redirect('back');
 });
 
 app.get('/delete-contact/',function(req,res){
     console.log(req.query);
-    let phone = req.query.phone;
-    let contactIndex = contactList.findIndex(contact=>contact.phone == phone);
-    if(contactIndex != -1){
-        contactList.splice(contactIndex,1);
-    }
-    return res.redirect('back');
+    // let phone = req.query.phone;
+    let id = req.query.id;
+    // let contactIndex = contactList.findIndex(contact=>contact.phone == phone);
+    // if(contactIndex != -1){
+    //     contactList.splice(contactIndex,1);
+    // }
+
+    Contact.findByIdAndDelete(id,function(err){
+        if(err){
+            console.log('error in deleting an contact');
+            return;
+        }
+
+        return res.redirect('back');
+    });
+
+    
 });
 
 app.listen(port,function(err){
